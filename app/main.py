16cbdd19c4801fd0fe573c85c60197529537a47f
Ambra <trank13f@gmail.com>
2026-01-09 15:02:04 +0100

    fix PEP8 annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,13 +11,13 @@ class Distance:
     def __repr__(self) -> str:
         return f'Distance(km={self.km})'
 
-    def __add__(self, other):
+    def __add__(self, other: 'Distance' | float | int) -> 'Distance':
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: 'Distance' | int | float) -> 'Distance':
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -22,23 +25,23 @@ class Distance:
             self.km += other
             return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: 'Distance' | int | float) -> 'Distance':
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: 'Distance' | int | float) -> 'Distance':
         return Distance(round(self.km / other, 2))
 
-    def __lt__(self, other):
+    def __lt__(self, other: 'Distance' | int | float) -> bool:
         return self.km < other
 
-    def __gt__(self, other):
+    def __gt__(self, other: 'Distance' | int | float) -> bool:
         return self.km > other
 
-    def __le__(self, other):
+    def __le__(self, other: 'Distance' | int | float) -> bool:
         return self.km <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other: 'Distance' | int | float) -> bool:
         return self.km >= other
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Distance' | int | float) -> bool:
         return self.km == other
