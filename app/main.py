09af942e78e6fc8aa26770676e7a5476f1cf02c9
Ambra <trank13f@gmail.com>
2026-01-09 14:08:55 +0100

    apply all math magic  methods and one str

--- app/main.py
+++ app/main.py
@@ -2,4 +2,22 @@ class Distance:
     def __init__(self, km: int) -> None:
         self.km = km
 
+    def __str__(self):
+        return f'Distance: {self.km} kilometers.'
+
+    def __repr__(self):
+        return f'Distance(km={self.km})'
+
+    def __add__(self, other):
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other):
+        self.km += other.km
+
+    def __mul__(self, other):
+        return Distance(self.km * other.km)
+
+    def __truediv__(self, other):
+        return Distance(self.km / other.km)
+
 
