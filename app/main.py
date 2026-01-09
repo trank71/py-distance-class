41749b2d5239c32f8895aadd22653b8f5d2fa58d
Ambra <trank13f@gmail.com>
2026-01-09 14:50:23 +0100

    fix logical methods

--- app/main.py
+++ app/main.py
@@ -29,16 +29,16 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
-        return self.km < other.km
+        return self.km < other
 
     def __gt__(self, other):
-        return self.km > other.km
+        return self.km > other
 
     def __le__(self, other):
-        return self.km <= other.km
+        return self.km <= other
 
     def __ge__(self, other):
-        return self.km >= other.km
+        return self.km >= other
 
     def __eq__(self, other):
-        return self.km == other.km
+        return self.km == other
