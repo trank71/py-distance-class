62a08440f057636164d3369bd40c6b04f7fa6515
Ambra <trank13f@gmail.com>
2026-01-09 15:21:36 +0100

    change type expected in mul and truediv methods

--- app/main.py
+++ app/main.py
@@ -25,10 +25,10 @@ class Distance:
             self.km += other
             return self
 
-    def __mul__(self, other: 'Distance' | int | float) -> 'Distance':
+    def __mul__(self, other: int | float) -> 'Distance':
         return Distance(self.km * other)
 
-    def __truediv__(self, other: 'Distance' | int | float) -> 'Distance':
+    def __truediv__(self, other: int | float) -> 'Distance':
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: 'Distance' | int | float) -> bool:
