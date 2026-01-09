eb81f1b852a3c344845204fdebf156b9d8028fa7
Ambra <trank13f@gmail.com>
2026-01-09 14:48:10 +0100

    fix math methods

--- app/main.py
+++ app/main.py
@@ -1,24 +1,32 @@
 class Distance:
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: float) -> None:
         self.km = km
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Distance: {self.km} kilometers.'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Distance(km={self.km})'
 
     def __add__(self, other):
-        return Distance(self.km + other.km)
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
 
     def __iadd__(self, other):
-        self.km += other.km
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        if isinstance(other, (int, float)):
+            self.km += other
+            return self
 
     def __mul__(self, other):
-        return Distance(self.km * other.km)
+        return Distance(self.km * other)
 
     def __truediv__(self, other):
-        return Distance(self.km / other.km)
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other):
         return self.km < other.km
