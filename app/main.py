7a11dd347b4840899c3a46eedb7748c83fb77b25
Ambra <trank13f@gmail.com>
2026-01-09 15:04:46 +0100

    fix logical type return

--- app/main.py
+++ app/main.py
@@ -32,16 +32,31 @@ class Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: 'Distance' | int | float) -> bool:
-        return self.km < other
+        if isinstance(other, Distance):
+            return self.km < other.km
+        if isinstance(other, (int, float)):
+            return self.km < other
 
     def __gt__(self, other: 'Distance' | int | float) -> bool:
-        return self.km > other
+        if isinstance(other, Distance):
+            return self.km > other.km
+        if isinstance(other, (int, float)):
+            return self.km > other
 
     def __le__(self, other: 'Distance' | int | float) -> bool:
-        return self.km <= other
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        if isinstance(other, (int, float)):
+            return self.km <= other
 
     def __ge__(self, other: 'Distance' | int | float) -> bool:
-        return self.km >= other
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        if isinstance(other, (int, float)):
+            return self.km >= other
 
     def __eq__(self, other: 'Distance' | int | float) -> bool:
-        return self.km == other
+        if isinstance(other, Distance):
+            return self.km == other.km
+        if isinstance(other, (int, float)):
+            return self.km == other
